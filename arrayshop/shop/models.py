4906a66e8e11973beaacc86fcdb0863b5d216606
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-06-24 17:52:21 +0100

    models connected to admin

--- arrayshop/shop/models.py
+++ arrayshop/shop/models.py
@@ -32,7 +32,10 @@ class Product(models.Model):
         null=True
     )
     description = models.TextField(blank=True)
-    price = models.DecimalField()
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
